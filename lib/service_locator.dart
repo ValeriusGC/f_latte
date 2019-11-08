95e176d80d343cc187a8bffa99e4a06018532931
valerius <frostyland@gmail.com>
2019-11-08 07:26:45 +0500

    init 0004

--- lib/service_locator.dart
+++
@@ -1,8 +0,0 @@
-import 'package:f_latte/text_manager.dart';
-import 'package:get_it/get_it.dart';
-
-GetIt sl = GetIt();
-
-void setUpServiceLocator() {
-  sl.registerSingleton<TextManager>(TextManager());
-}
